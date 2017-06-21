17fa663d5758e20f23591fb3912f254ebc0630d0
Vinai Kopp <vinai@netzarbeiter.com>
2017-06-21 06:14:43 +0100

    Add PHPDoc annotations

--- Model/State/InMemoryGameState.php
+++ Model/State/InMemoryGameState.php
@@ -11,17 +11,26 @@ class InMemoryGameState implements GameStateInterface
     private $targetColors = [];
     
     private $guessCount = 0;
-    
+
+    /**
+     * @param string[] $colors
+     */
     public function setTargetColors(array $colors)
     {
         $this->targetColors = $colors;
     }
 
+    /**
+     * @return string[]
+     */
     public function getTargetColors()
     {
         return $this->targetColors;
     }
 
+    /**
+     * @return int
+     */
     public function getGuessCount()
     {
         return $this->guessCount;
